de60963f675c498da7c2ad6565d7c35ecf7b5eed
Nelson Roberts <paraglade@gmail.com>
2026-03-21 06:47:43 -0700

    fixing documentation generation

--- main.go
+++ main.go
@@ -1,6 +1,4 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .
 package main
 
 import (
